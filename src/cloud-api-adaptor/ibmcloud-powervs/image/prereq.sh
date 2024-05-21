6271d75b21627ec9be00410c1324d9b33596dda6
Shahid <Shahid.Shaikh5@ibm.com>
2024-05-21 11:13:46 +0530

    Update prereq.sh

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -2,10 +2,13 @@
 
 GO_VERSION="1.21.10"
 RUST_VERSION="1.72.0"
-SKOPEO_VERSION="1.5.0"
 
 # Install dependencies
-yum install -y curl protobuf-compiler libseccomp-devel openssl openssl-devel perl skopeo-${SKOPEO_VERSION}
+yum install -y curl protobuf-compiler libseccomp-devel openssl openssl-devel perl skopeo clang clang-devel
+
+#Install device-mapepr-devel
+wget https://rpmfind.net/linux/centos/8-stream/PowerTools/ppc64le/os/Packages/device-mapper-devel-1.02.181-14.el8.ppc64le.rpm
+yum install -y device-mapper-devel-1.02.181-14.el8.ppc64le.rpm
 
 # Install Golang
 curl https://dl.google.com/go/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
